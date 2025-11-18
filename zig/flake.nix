6304c347b5227ca0e895bc4e98cd9bb30185bd29
neo <glassesneo@protonmail.com>
2025-11-19 00:07:49 +0900

    update zig flake

--- zig/flake.nix
+++ zig/flake.nix
@@ -8,12 +8,6 @@
       };
     };
     zls-overlay.url = "github:zigtools/zls/0.15.0";
-    zon2nix = {
-      url = "github:jcollie/zon2nix";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
   };
 
   outputs = {
@@ -21,7 +15,7 @@
     zig-overlay,
     zls-overlay,
     ...
-  } @ inputs: let
+  }: let
     allSystems = [
       "aarch64-darwin"
       "x86_64-darwin"
@@ -36,13 +30,11 @@
         zls = zls-overlay.packages.${system}.zls.overrideAttrs (old: {
           nativeBuildInputs = [zig];
         });
-        zon2nix = inputs.zon2nix.packages.${system}.zon2nix;
       in
         pkgs.mkShell {
           packages = [
             zig
             zls
-            zon2nix
           ];
         }
     );
@@ -52,9 +44,9 @@
     # default = pkgs.stdenv.mkDerivation {
     # pname = "zig-template";
     # version = "0.1.0";
-    # src = ../.;
+    # src = ./.;
     # nativeBuildInputs = [
-    # pkgs.zig_0_14.hook
+    # pkgs.zig_0_15.hook
     # ];
     # };
     # }
