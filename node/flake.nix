f5a8e9ca7bc852a711ee01b35da3212598385e94
neo <glassesneo@protonmail.com>
2026-06-23 14:33:15 +0900

    feat(node): update nodejs template

--- node/flake.nix
+++ node/flake.nix
@@ -21,16 +21,92 @@
         inputs.treefmt-nix.flakeModule
       ];
       systems = import systems;
-      perSystem = {pkgs, ...}: {
+      perSystem = {
+        pkgs,
+        lib,
+        self',
+        ...
+      }: let
+        pname = "";
+        version = "0.1.0";
+        src = lib.cleanSource ./.;
+
+        nodejs = pkgs.nodejs_26;
+        pnpm = pkgs.pnpm_11;
+        pnpmDeps = pkgs.fetchPnpmDeps {
+          inherit pname version src;
+          fetcherVersion = 4;
+          hash = lib.fakeHash;
+        };
+        mkPnpmDerivation = attrs:
+          pkgs.stdenv.mkDerivation ({
+              inherit pname version src pnpmDeps;
+
+              nativeBuildInputs = [
+                nodejs
+                pnpm
+                pkgs.pnpmConfigHook
+              ];
+            }
+            // attrs);
+
+        emptyInstallPhase = ''
+          mkdir -p $out
+          touch $out/done
+        '';
+      in {
+        packages.default = mkPnpmDerivation {
+          buildPhase = ''
+            runHook preBuild
+            pnpm run build
+            runHook postBuild
+          '';
+
+          installPhase = ''
+            runHook preInstall
+            mkdir -p $out
+            cp -r dist $out/
+            runHook postInstall
+          '';
+        };
+        checks = {
+          build = self'.packages.default;
+
+          typecheck = mkPnpmDerivation {
+            pname = "${pname}-typecheck";
+
+            buildPhase = ''
+              runHook preBuild
+              pnpm run typecheck
+              runHook postBuild
+            '';
+
+            installPhase = emptyInstallPhase;
+          };
+
+          lint = mkPnpmDerivation {
+            pname = "${pname}-lint";
+
+            buildPhase = ''
+              runHook preBuild
+              pnpm run lint
+              runHook postBuild
+            '';
+
+            installPhase = emptyInstallPhase;
+          };
+        };
+
         devShells = {
           default = pkgs.mkShellNoCC {
-            packages = with pkgs; [
+            packages = [
               nodejs
               pnpm
-              typescript-language-server
+              pkgs.typescript-language-server
             ];
           };
         };
+
         treefmt = {
           projectRootFile = "flake.nix";
           programs = {
